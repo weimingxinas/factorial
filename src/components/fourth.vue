bb1c635ecb2409f392a922a258bc3476b0c939c8
wmx <1043426948@qq.com>
2019-09-10 00:39:41 +0800

    feat: tail call

--- src/components/fourth.vue
+++ src/components/fourth.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="second">
+  <div class="fourth">
+    <h3>尾递归</h3>
+    <section>
+      <p>函数调用会在内存形成一个"调用记录"，又称"调用帧"（call frame），保存调用位置和内部变量等信息。如果在函数A的内部调用函数B，
+      那么在A的调用记录上方，还会形成一个B的调用记录。等到B运行结束，将结果返回到A，B的调用记录才会消失。如果函数B内部还调用函数C，
+      那就还有一个C的调用记录栈，以此类推。所有的调用记录，就形成一个"调用栈"</p>
+      <p>尾递归由于是函数的最后一步操作，所以不需要保留外层函数的调用记录，
+      因为调用位置、内部变量等信息都不会再用到了，只要直接用内层函数的调用记录，取代外层函数的调用记录就可以了</p>
+    </section>
     <h3>code</h3>
     <section>
       <pre>
@@ -11,14 +19,11 @@
             if (n && isNaN(n)) {
               throw "parameter is not a number!";
             }
-            if (n&lt;=0) {
+            if (n<=0) {
               throw "parameter is not a positive integer!";
             }
-            var res = 1n;
-            for(let i= 1;i&lt;= n;i++){
-              res = res * i;
-            }
-            return res;
+            if (n === 1) return total; 
+            return factorial(n - 1, BigInt(n) * BigInt(total));
           }
         </code>
       </pre>      
@@ -42,6 +47,13 @@
           v-model="result">
         </el-input>
     </section>
+    <h3>注意!</h3>
+    <section>
+      <p>Chrome下使用尾递归写法的方法依旧出现调用栈溢出，原因在于：</p>
+      <ol>
+        <li>各大浏览器（除了safari）根本就没部署尾调用优化</li>
+      </ol>
+    </section>
   </div>
 </template>
 
@@ -55,7 +67,7 @@ export default {
     }
   },
   methods: {
-    factorial(n){
+    factorial(n, total = 1){
       if (!n && n !== 0) {
         throw "No parameter exists";
       }
@@ -65,12 +77,8 @@ export default {
       if (n<=0) {
         throw "parameter is not a positive integer!";
       }
-      const num = BigInt(n);
-      let res = BigInt(1);
-      for(let i= BigInt(1);i<= num;i++){
-        res = res * i;
-      }
-      return res;
+      if (n === 1) return total;
+      return this.factorial(n - 1, BigInt(n) * BigInt(total));
     },
     calc() {
       const start = (new Date()).getTime();
